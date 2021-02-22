7567b8afd22a3702955e91f12b0062e670e03964
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-02-22 18:06:56 +0800

    style:

--- src/util.ts
+++ src/util.ts
@@ -21,11 +21,11 @@ export function parseSizeAndType(txt: string) {
 }
 const baseArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
 export function randomFileName() {
-    const length = 5 + (Math.random() * 10|0)
+    const length = 5 + (Math.random() * 10 | 0)
     const baseArrayLength = baseArray.length
     const array = new Array(length)
     for (let i = 0; i < length; i++) {
-        array[i] = baseArray[Math.random() * baseArrayLength|0]
+        array[i] = baseArray[Math.random() * baseArrayLength | 0]
     }
-    return array.join('')+'.jpg'
+    return array.join('') + '.jpg'
 }
