f2a28a93bb2d79ef34748f2fd71581f48df91124
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-06-11 19:30:34 +0800

    添加声明

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * iqdb-client
+ * @description iqdb.org api client for Node.js.
+ * @version 1.0.4
+ * @author KotoriK
+ * @license MIT
+ */
 import searchPic from './api'
 export { IQDBSearchResultItem, Size, IQDB_OPTIONS, setIQDBOptions } from './api'
 export { searchPic }
