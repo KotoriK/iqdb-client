776f09ad8fcd2884840d5e8865d6738d575eb905
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-01-20 20:44:52 +0800

    fix test

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,35 +1,33 @@
 import searchPic from "../src/api";
 import fs from 'fs/promises'
 import { resolve } from 'path'
+jest.setTimeout(30000);
+
 test('find by url', async () => {
-    jest.setTimeout(30000);
     const result = (await searchPic('https://pixiv.cat/84035784-3.jpg', { lib: 'www' }))
     if (!result.ok) throw result
     expect(result.data.length).toBeGreaterThan(0)
 
 })
 test('find by url, single-lib search', async () => {
-    jest.setTimeout(30000);
     const result = (await searchPic('https://pixiv.cat/84035784-3.jpg', { lib: 'danbooru' }))
     if (!result.ok) throw result
     expect(result.data.length).toBeGreaterThan(0)
 
 })
 test('find by url, 3diqdb', async () => {
-    jest.setTimeout(30000);
     const result = (await searchPic('https://pixiv.cat/84035784-3.jpg', { lib: '3d' }))
     expect(result.ok).toBe(false)
     expect(result.data.length).toBeGreaterThan(0)
 })
 test('find by url, multilib', async () => {
-    jest.setTimeout(30000);
     const result = (await searchPic('https://pixiv.cat/84035784-3.jpg', { lib: 'www', libs: [1, 2] }))
     if (!result.ok) throw result
     expect(result.data.length).toBeGreaterThan(0)
 })
 test('find by local res', async () => {
-    jest.setTimeout(30000);
     const result = (await searchPic(await fs.readFile(resolve(__dirname, '../test/84035784_p2.jpg')), { lib: 'www' }))
     console.log(result)
+    if (!result.ok) throw result
     expect(result.data.length).toBeGreaterThan(0)
 })
