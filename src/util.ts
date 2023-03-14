5ce0f7d2bc9489e0a532ce41d36648f6dcd47e43
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-03-14 09:00:22 +0800

    perf: use string

--- src/util.ts
+++ src/util.ts
@@ -23,13 +23,13 @@ export function parseSizeAndType(txt: string): { size: Size, type: IQDB_RESULT_T
         return txt
     }
 }
-const baseArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
+const availCharset = '0123456789abcdefghijklmnopqrstuvwxyz@ABCDEFGHIJKLMNOPQRSTUVWXYZ_'
+const availCharsetLen = availCharset.length
 export function getRandomName() {
     const length = 5 + (Math.random() * 10 | 0)
-    const baseArrayLength = baseArray.length
     let str = ''
     for (let i = 0; i < length; i++) {
-        str += baseArray[Math.random() * baseArrayLength | 0]
+        str += availCharset[Math.random() * availCharsetLen | 0]
     }
     return str + '.jpg'
 }
